1afd3ac0ac4296ab736beda764e58279be5ffe88
michaellh <Michael_Hoang@live.ca>
2016-06-09 00:12:58 -0600

    Readjusted the testmenu code to use Game and Screen and added a GameplayScreen class for actual gameplay after touching the smiley face from the main menu

---
+++ core/src/com/badlogic/gamescreentest/GameplayScreen.java
@@ -0,0 +1,124 @@
+package com.badlogic.gamescreentest;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+/**
+ * Created by admin on 6/8/2016.
+ */
+public class GameplayScreen implements Screen, InputProcessor {
+    final Game game;
+    OrthographicCamera camera;
+    private Sprite backGround;
+    Stage stage;
+
+    float cellWidth;
+    float cellHeight;
+    float screenWidth;
+    float screenHeight;
+
+    public GameplayScreen(final GameScreen gam) {
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+        cellWidth = screenWidth / 5;
+        cellHeight = screenHeight / 3;
+
+        this.game = gam;
+        camera = new OrthographicCamera();
+        stage = new Stage(new ScreenViewport(camera));      //FIX PROBLEM WITH 2 SPRITEBATCHES
+
+        backGround = new Sprite(new Texture(Gdx.files.internal("ugly face sean 2.png")));
+        backGround.setPosition(0, 0);
+        backGround.setSize(cellWidth * 5, cellHeight * 3);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.getBatch().begin();
+        backGround.draw(stage.getBatch());
+        stage.getBatch().end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
---
+++ core/src/com/badlogic/gamescreentest/MainMenuScreen.java
@@ -0,0 +1,165 @@
+package com.badlogic.gamescreentest;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+/**
+ * Created by admin on 6/8/2016.
+ */
+public class MainMenuScreen implements Screen, InputProcessor {
+    final GameScreen game;
+    OrthographicCamera camera;
+    Stage stage;
+
+    Vector2 storeTouch;
+    float cellWidth;
+    float cellHeight;
+    float screenWidth;
+    float screenHeight;
+
+    private Sprite backGround;
+    private Sprite sadFace;
+    private Sprite neutralFace;
+    private Sprite happyFace;
+
+    public MainMenuScreen(final GameScreen gam) {
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+        cellWidth = screenWidth / 5;
+        cellHeight = screenHeight / 3;
+
+        game = gam;
+        camera = new OrthographicCamera();
+        stage = new Stage(new ScreenViewport(camera), game.batch);
+
+        backGround = new Sprite(new Texture(Gdx.files.internal("ugly face sean.jpg")));
+        backGround.setPosition(0, 0);
+        backGround.setSize(cellWidth * 5, cellHeight * 3);
+
+        neutralFace = new Sprite(new Texture(Gdx.files.internal("neutralface.jpg")));
+        neutralFace.setPosition(cellWidth * 1, cellHeight);
+        neutralFace.setSize(cellWidth, cellHeight);
+
+        sadFace = new Sprite(new Texture(Gdx.files.internal("sadface.jpg")));
+        sadFace.setPosition(cellWidth * 2, cellHeight);
+        sadFace.setSize(cellWidth, cellHeight);
+
+        happyFace = new Sprite(new Texture(Gdx.files.internal("happyface.jpg")));
+        happyFace.setPosition(cellWidth * 3, cellHeight);
+        happyFace.setSize(cellWidth, cellHeight);
+
+        InputMultiplexer im = new InputMultiplexer(stage, this);
+        Gdx.input.setInputProcessor(im);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.getBatch().begin();
+        backGround.draw(stage.getBatch());
+        neutralFace.draw(stage.getBatch());
+        sadFace.draw(stage.getBatch());
+        happyFace.draw(stage.getBatch());
+        stage.getBatch().end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println(Gdx.input.getX() + " " + (stage.getHeight() - Gdx.input.getY()));
+        storeTouch = new Vector2(Gdx.input.getX(), (stage.getHeight() - Gdx.input.getY()));
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        if(storeTouch.x >= neutralFace.getX() && storeTouch.x <= neutralFace.getX() + cellWidth
+                && storeTouch.y >= neutralFace.getY() && storeTouch.y <= neutralFace.getY() + cellHeight) {
+            System.out.println("neutralface.jpg touched");
+        }
+        else if(storeTouch.x >= sadFace.getX() && storeTouch.x <= sadFace.getX() + cellWidth
+                && storeTouch.y >= sadFace.getY() && storeTouch.y <= sadFace.getY() + cellHeight) {
+            System.out.println("sadface.jpg touched");
+            Gdx.app.exit();
+        }
+        else if(storeTouch.x >= happyFace.getX() && storeTouch.x <= happyFace.getX() + cellWidth
+                && storeTouch.y >= happyFace.getY() && storeTouch.y <= happyFace.getY() + cellHeight) {
+            System.out.println("happyface.jpg touched");
+            game.setScreen(new GameplayScreen(game));
+            dispose();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
