1caa412cc2ec85fe37a752f783157067c0789f5b
michaellh <Michael_Hoang@live.ca>
2016-06-08 22:12:39 -0600

    Base files

---
+++ desktop/src/com/badlogic/gamescreentest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badlogic.gamescreentest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gamescreentest.GameScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameScreen(), config);
+	}
+}
