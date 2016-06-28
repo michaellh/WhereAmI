5cbefc59df456dcf66fe2ae5995521edd2c735ad
michaellh <Michael_Hoang@live.ca>
2016-06-27 21:46:49 -0600

    Not sure what changed since the last time. Perhaps the fact that I left the random wall generation at 40% instead of the recommended 45%?

--- core/src/com/badlogic/gamescreentest/GameplayScreen.java
+++ core/src/com/badlogic/gamescreentest/GameplayScreen.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import java.util.Random;
 
@@ -20,8 +19,8 @@ public class GameplayScreen implements Screen, InputProcessor {
     OrthographicCamera camera;
     Stage stage;
     Sprite backGround;
-
     Random rand;
+
     int wallCount;
     int[][] ogMap;
     int[][] newMap;
